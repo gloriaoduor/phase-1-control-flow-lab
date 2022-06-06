0a008fb22ff8da3a2b199976b6e9f3ad2367d29f
gloria oduor <gloriaoduor@gmail.com>
2022-06-06 17:13:11 +0300

    Control flow lab solution

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400 ){
+    return( "This one is on me!");
+  }
+    else if (2000 < someValue && someValue < 2500){
+      return "I will gladly take your thirty bucks.";
+    } 
+      else {return "No can do."}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+function ternaryCheckCity(destination){
+  const city = "NYC"
+  const message  = destination == city ? 'Ok, sounds good.' : 'No go.'
+  return message;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+      break;
+  }
+  
 }
